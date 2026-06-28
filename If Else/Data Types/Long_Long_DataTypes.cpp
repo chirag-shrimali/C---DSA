d667a5074a67063914ab238d90a2cdda54a4b87f
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-28 23:33:16 +0530

    Long Long Data Types

---
+++ If Else/Data Types/Long_Long_DataTypes.cpp
@@ -0,0 +1,18 @@
+// Boiler Plate Code...
+
+#include <iostream>
+
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    long long x = LLONG_MAX , y = LLONG_MIN;
+
+    // long long y = LLONG_MIN;
+
+    cout << x << endl << y;
+
+    return 0;
+}
