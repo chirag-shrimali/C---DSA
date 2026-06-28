3de5f24cb66346e55b55e4a6d3ffe5dffbfe9f14
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-06-28 23:20:12 +0530

    Range Of INT

---
+++ If Else/Range/Range_Of_INT.cpp
@@ -0,0 +1,30 @@
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int x1 = INT8_MAX;
+
+    // cout << x1 << endl;
+
+    // int x2 = INT16_MAX;
+
+    // cout << x2 << endl;
+
+    int max = INT32_MAX;
+
+    cout << max << endl;
+
+    int min = INT32_MIN;
+
+    cout << min;
+
+    // int x4 = INT64_MAX;
+
+    // cout << x4 << endl;
+
+    return 0;
+}
