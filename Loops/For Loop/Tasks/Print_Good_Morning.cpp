d197f844a3d72534639756d01b1a29b8432233cc
Chirag Shrimali <chiragshrimali0411@gmail.com>
2026-07-02 01:49:58 +0530

    Prints Good Morning For Loops

---
+++ Loops/For Loop/Tasks/Print_Good_Morning.cpp
@@ -0,0 +1,41 @@
+// Prints 'Good Morning' 10 times...
+
+// Boiler Plate Code...
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!" << endl;
+
+    // cout << "Good Morning!!";
+
+    // ---------------------------------------------------------------------------------
+
+    int i;
+
+    for(i = 1 ; i <= 10 ; i++)
+    {
+        cout << "Good Morning!!" << endl;
+    }
+
+    return 0;
+}
